c61b5387d03337f0bc3f61d538e418a5b42e657b
Yannik Hahn <yannihahn@t-online.de>
2023-04-24 16:04:31 +0200

    Reenables ManyToMany Pivot tables and Composite Key entities

---
+++ backend/src/main/java/com/itsziroy/weingutmerlot/backend/db/entities/pivot/GaerungsprozessschritteHasHefen.java
@@ -0,0 +1,59 @@
+package com.itsziroy.weingutmerlot.backend.db.entities.pivot;
+
+import com.itsziroy.weingutmerlot.backend.db.entities.Gaerungsprozessschritt;
+import com.itsziroy.weingutmerlot.backend.db.entities.Hefe;
+import com.itsziroy.weingutmerlot.backend.db.entities.pivot.compositeKeys.GaerungsprozessschritteHasHefenId;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "gaerungsprozessschritte_has_hefen")
+public class GaerungsprozessschritteHasHefen {
+    @EmbeddedId
+    private GaerungsprozessschritteHasHefenId id;
+
+    @MapsId("gaerungsprozessschritteId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "gaerungsprozessschritte_id", nullable = false)
+    private Gaerungsprozessschritt gaerungsprozessschritt;
+
+    @MapsId("hefenId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "hefen_id", nullable = false)
+    private Hefe hefe;
+
+    @Column(name = "menge", nullable = false)
+    private Double menge;
+
+    public GaerungsprozessschritteHasHefenId getId() {
+        return id;
+    }
+
+    public void setId(GaerungsprozessschritteHasHefenId id) {
+        this.id = id;
+    }
+
+    public Gaerungsprozessschritt getGaerungsprozessschritt() {
+        return gaerungsprozessschritt;
+    }
+
+    public void setGaerungsprozessschritt(Gaerungsprozessschritt gaerungsprozessschritt) {
+        this.gaerungsprozessschritt = gaerungsprozessschritt;
+    }
+
+    public Hefe getHefe() {
+        return hefe;
+    }
+
+    public void setHefe(Hefe hefe) {
+        this.hefe = hefe;
+    }
+
+    public Double getMenge() {
+        return menge;
+    }
+
+    public void setMenge(Double menge) {
+        this.menge = menge;
+    }
+
+}
---
+++ backend/src/main/java/com/itsziroy/weingutmerlot/backend/db/entities/pivot/UeberpruefungenHasHefen.java
@@ -0,0 +1,62 @@
+package com.itsziroy.weingutmerlot.backend.db.entities.pivot;
+
+import com.itsziroy.weingutmerlot.backend.db.entities.Hefe;
+import com.itsziroy.weingutmerlot.backend.db.entities.Ueberpruefung;
+import com.itsziroy.weingutmerlot.backend.db.entities.pivot.compositeKeys.UeberpruefungenHasHefenId;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ueberpruefungen_has_hefen")
+public class UeberpruefungenHasHefen {
+    @EmbeddedId
+    private UeberpruefungenHasHefenId id = new UeberpruefungenHasHefenId();
+
+    @MapsId("ueberpruefungenId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ueberpruefungen_id", nullable = false)
+    private Ueberpruefung ueberpruefung;
+
+    @MapsId("hefenId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "hefen_id", nullable = false)
+    private Hefe hefe;
+
+    @Column(name = "anpassung", nullable = false)
+    private Double anpassung;
+
+    public UeberpruefungenHasHefenId getId() {
+        return id;
+    }
+
+    public void setId(UeberpruefungenHasHefenId id) {
+        this.id = id;
+    }
+
+    public Ueberpruefung getUeberpruefung() {
+        return ueberpruefung;
+    }
+
+    public void setUeberpruefung(Ueberpruefung ueberpruefung) {
+        this.ueberpruefung = ueberpruefung;
+        this.id.setUeberpruefungenId(ueberpruefung.getId());
+    }
+
+    public Hefe getHefe() {
+        return hefe;
+    }
+
+    public void setHefe(Hefe hefe) {
+        this.hefe = hefe;
+        this.id.setHefenId(hefe.getId());
+    }
+
+
+    public Double getAnpassung() {
+        return anpassung;
+    }
+
+    public void setAnpassung(Double anpassung) {
+        this.anpassung = anpassung;
+    }
+
+}
